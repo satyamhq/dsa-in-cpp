734d5bf772353b27bae55de895bb4aedc395c8f3
Satyam Kumar <satyam31sk@gmail.com>
2026-08-22 15:52:44 +0530

    add 12-user-input.cpp

--- Tutorial/01-Basics/12-user-input.cpp
+++ Tutorial/01-Basics/12-user-input.cpp
@@ -8,17 +8,19 @@ int main(){
     cout << "Enter y: ";
     cin >> y;
 
-    int addition, subtraction, multiplication, division;
+    int addition, subtraction, multiplication, division, modulus;
 
     addition = x + y;
     subtraction = x - y;
     multiplication = x * y;
     division = x / y;
+    modulus = x % y;  // modulus operator
     
     cout << "addition: " << addition << endl;
     cout << "subtraction: " << subtraction << endl;
     cout << "multiplication: " << multiplication << endl;
     cout << "division: " << division << endl;
+    cout << "modulus: " << modulus << endl;
 
     return 0;
 }
