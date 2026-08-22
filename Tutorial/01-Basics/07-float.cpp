f5bea6e59447eaee71bc19629fe612692d4526fa
Satyam Kumar <satyam31sk@gmail.com>
2026-08-22 15:00:35 +0530

    add 07-float.cpp

---
+++ Tutorial/01-Basics/07-float.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float x = 5, y = 2;
+    cout << x + y << endl;  // 7
+    cout << x - y << endl;  // 3
+    cout << x * y << endl;  // 10
+    cout << x / y << endl;  // 2.5
+
+    return 0;
+}
