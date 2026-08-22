ee385c4e994359c01d58065631a1de73ee2348db
Satyam Kumar <satyam31sk@gmail.com>
2026-08-22 18:10:54 +0530

    add 05-three-digit-or-not.cpp

---
+++ Tutorial/02-Conditionals/05-three-digit-or-not.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    if (n >= 99 and n <= 999){
+        cout << "The number: " << n << " is a three digit number." << endl;
+    } else{
+        cout << "The number: " << n << " is not a three digit number." <<endl; 
+    }
+    return 0;
+}
