16b4c745ea833aedcb7cefc862d6c5cb5895dc64
Satyam Kumar <satyam31sk@gmail.com>
2026-08-22 14:30:21 +0530

    03-printing-numbers.cpp

---
+++ Tutorial/01-Basics/03-printing-numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "4" << endl;  // 4
+    cout << 4 << endl;    // 4
+    cout << 4+3 <<endl;   // 3
+    cout << "4+3" <<endl; // 4+3
+     cout << 4*3 << endl; // 12
+    cout << "4*3";        // 4*3
+
+    return 0;
+}
